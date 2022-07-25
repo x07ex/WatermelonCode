449aa6edc5c393b46bd4329f847648b05893c3bb
x07exploit <0x7ex@protonmail.com>
2022-07-25 12:37:51 -0400

    Update 1.py

--- Week2/1.py
+++ Week2/1.py
@@ -1,3 +1,12 @@
+"""
+8.- Grilla perfecta
+
+A Andrea le encantan los arreglos bidimensionales, y ha inventado un juego para jugar con sus amigas, en esté se tendrá que identificar si una grilla es perfecta, ¿Cuándo una grilla es perfecta? Cuando tiene números 1 cruzando la matriz diagonalmente hacia un lado o otro.
+Se sabe que la matriz siempre será de un largo y alto impar
+
+Tu tarea es crear un programa el cuál identifique si la matriz es perfecta.
+"""
+
 def _main(num: int, matrix: tuple or list) -> str:
     matrix = matrix.split('\n')
     if num % 2 != 0 and len(matrix[0]) % 2 != 0:
@@ -5,4 +14,4 @@ def _main(num: int, matrix: tuple or list) -> str:
     else:
         return f"{matrix}\nNo"
 
-print(_main(3, '\n'.join(['1 0 0', '0 0 1', '0 1 0'])))
+_main(3, '\n'.join(['1 0 0', '0 0 1', '0 1 0']))
