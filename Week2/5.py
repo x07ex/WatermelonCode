8ba863b37f693c1750b36dc056d8422f3a982c11
x07exploit <0x7ex@protonmail.com>
2022-07-29 13:21:04 -0400

    Add files via upload

---
+++ Week2/5.py
@@ -0,0 +1,34 @@
+"""
+12.- Is...?
+
+Tu tarea será hacer un programa que reciba un string S, el programa debe identificar si está en lowercase, o uppercase, además de esto, deberá cambiarlo a su contrario, pero sin usar los upper() y lower() de dicho lenguaje de programación, con esto me refiero a que esta prohibido utilizar los métodos predefinidos en el lenguaje a usar.
+S solo puede ser lowercase o UPPERCASE, no mixto.
+"""
+
+def toUpperCase(txt: str) -> str:
+    newText = ""
+    for x in txt:
+        if ord(x) > 96 \
+            and ord(x) < 123:
+                newText += chr(ord(x) - 32)
+        else:
+            newText += x
+    return newText
+
+def toLowerCase(txt: str) -> str:
+    newText = ""
+    for x in txt:
+        if ord(x) > 64 \
+            and ord(x) < 91:
+                newText += chr(ord(x) + 32)
+        else:
+            newText += x
+    return newText
+
+def _main(txt: str) -> int:
+    if txt.islower():
+        print("LowerCase\n" + toUpperCase(txt))
+    else:
+        print("UpperCase\n" + toLowerCase(txt))
+
+_main("watermelon")
